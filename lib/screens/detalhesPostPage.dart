ee1934142d43cb9b4842d3ddf2a72ba76212dc45
TiagoRuaroMezzomo <tiagomezzruaro@gmail.com>
2024-01-28 15:46:13 -0300

    teste_git

--- lib/screens/detalhesPostPage.dart
+++ lib/screens/detalhesPostPage.dart
@@ -22,7 +22,7 @@ class DetalhesPostPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detalhes do Post'),
+        title: Text('Detalhes do Post!'),
       ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
